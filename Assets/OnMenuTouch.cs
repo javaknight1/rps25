a1b6cca33775161f25a58a2fffc6d2fe07771c32
Rob Avery <robertaveryiv@gmail.com>
2018-08-31 17:54:29 -0400

    New Game button switches scene to game scene

---
+++ Assets/OnMenuTouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnMenuTouch : MonoBehaviour {
+
+	// Use this for initialization
+	public void OnNewGameButtonTouch () {
+        SceneManager.LoadScene("Game");
+	}
+
+    public void OnSettingsButtonTouch(){
+        // TODO: Implement function
+    }
+
+    public void OnHighscoreButtonTouch(){
+        // TODO: Implement function
+    }
+		
+}
