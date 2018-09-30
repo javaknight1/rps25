bd34ff857f631c49db4c2f039180ca91fc18cfbb
Rob Avery <robertaveryiv@gmail.com>
2018-09-29 20:14:15 -0400

    Created panel for game results

---
+++ Assets/Scripts/YourWeapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New YourWeapon", menuName = "YourWeapon")]
+public class YourCard : ScriptableObject
+{
+
+    public string weaponName;
+    public Sprite artwork;
+
+}
