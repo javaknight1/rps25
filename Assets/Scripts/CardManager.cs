33be1d2f0dad9ae90e7a6c7a6ce2e3f68e7a2ed6
Rob Avery <robertaveryiv@gmail.com>
2018-12-25 00:33:11 -0500

    Added CardManager and GameSession

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CardManager : MonoBehaviour {
+
+    private WeaponData[] weapons;
+    private const string weaponsListFileName = "weapons.json";
+
+    // Use this for initialization
+    void Start () {
+        string filePath = Path.Combine(Application.streamingAssetsPath, weaponsListFileName);
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            WeaponsData weaponData = WeaponsData.CreateFromJSON(dataAsJson);
+            this.weapons = weaponData.weapons;
+        }
+        else
+        {
+            // TODO: Handle an error case here
+            this.weapons = new WeaponData[0];
+        }
+    }
+
+    public int numOfWeapons()
+    {
+        return weapons.Length;
+    }
+	
+    public string getWeaponName(int index)
+    {
+        return weapons[index].weapon;
+    }
+
+    public string getWeaponDescription(int index)
+    {
+        return weapons[index].description;
+    }
+
+    public Sprite getWeaponArtworkSprite(int index)
+    {
+        return Resources.Load<Sprite>("Sprites/Weapons/" + weapons[index].weapon);
+    }
+	
+}
---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour {
+
+	public Card pc;
+	public Card user;
+
+	private int loses = 0;
+	private int wins = 0;
+	private int ties = 0;
+
+	public void selectedCard(Card pc)
+    {
+		// TODO: Check who wins and add to counter
+	}
+}
