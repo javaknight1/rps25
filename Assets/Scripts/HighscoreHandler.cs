b1cf24003b36e5de4b059ddbea3fb30b3a456895
Rob Avery <robertaveryiv@gmail.com>
2019-01-13 17:43:17 -0500

    Created page to view highscore stats

---
+++ Assets/Scripts/HighscoreHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreHandler : MonoBehaviour {
+
+    public Text sessionWins;
+    public Text sessionLoses;
+    public Text sessionTies;
+
+    public Text totalWins;
+    public Text totalLoses;
+    public Text totalTies;
+
+    public Text topComputerWeapon;
+    public Text topUserWeapon;
+
+    // Use this for initialization
+    void Start () {
+        sessionWins.text = PlayerPrefs.GetInt(PlayerPrefKeys.SESSIONWINS).ToString();
+        sessionLoses.text = PlayerPrefs.GetInt(PlayerPrefKeys.SESSIONLOSES).ToString();
+        sessionTies.text = PlayerPrefs.GetInt(PlayerPrefKeys.SESSIONTIES).ToString();
+
+        totalWins.text = PlayerPrefs.GetInt(PlayerPrefKeys.TOTALWINS).ToString();
+        totalLoses.text = PlayerPrefs.GetInt(PlayerPrefKeys.TOTALLOSES).ToString();
+        totalTies.text = PlayerPrefs.GetInt(PlayerPrefKeys.TOTALTIES).ToString();
+
+        //sessionWins.text = PlayerPrefs.GetInt(PlayerPrefKeys.SESSIONWINS).ToString();
+        //sessionWins.text = PlayerPrefs.GetInt(PlayerPrefKeys.SESSIONWINS).ToString();
+    }
+}
