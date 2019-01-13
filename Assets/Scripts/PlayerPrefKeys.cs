ab8417695bb4439b6d98c50a8b9c3360ab9b3f5c
Rob Avery <robertaveryiv@gmail.com>
2019-01-13 17:41:03 -0500

    Created helper class for generating keys for PlayerPrefs

--- Assets/Scripts/PlayerPrefKeys.cs
+++ Assets/Scripts/PlayerPrefKeys.cs
@@ -5,19 +5,23 @@ using System;
 
 public class PlayerPrefKeys
 {
+    /* Total count of wins/loses/ties of single games */
     public const string TOTALWINS = "TotalWins";
     public const string TOTALLOSES = "TotalLoses";
     public const string TOTALTIES = "TotalTies";
 
+    /* Total count of wins/loses/ties of sessions */
     public const string SESSIONWINS = "SessionWins";
     public const string SESSIONLOSES = "SessionLoses";
     public const string SESSIONTIES = "SessionTies";
 
+    /* Count the number of times each weapon is used by the computer */
     public static string ComputerWeaponKey(string weapon)
     {
         return "Computer-" + weapon;
     }
 
+    /* Count the number of times each weapon is used by the user */
     public static string PlayerWeaponKey(string weapon)
     {
         return "Player-" + weapon;
